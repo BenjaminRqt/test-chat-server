4e6f64093e12418e283681a05f6e17f879fd71fb
Benjamin Rouquet <benjamin.rouquet4@gmail.com>
2020-09-30 15:53:09 +0100

    connection jwt user

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const userService = require("../services/user.service")
+const auth = require('../middleware/auth');
 
-router.get("/users", userService.getAll.bind(userService))
+router.get("/users", auth, userService.getAll.bind(userService))
 router.get("/users/:username", userService.getByUsername.bind(userService))
 router.post("/users", userService.create.bind(userService))
+router.post("/login", userService.login.bind(userService))
 
 module.exports = router
